69283791c3d280bb2dd5220294eb412ce9d6e0e3
Gaurav Panchal <gkp.125@gmail.com>
2024-04-24 20:43:17 +0530

    Job Submitted Page added

---
+++ src/app/jobs/new/actions.ts
@@ -0,0 +1,61 @@
+"use server";
+
+import { toSlug } from "@/lib/utils";
+import { CreateJobSchema } from "@/lib/validations/jobfilters.schema";
+import { nanoid } from "nanoid";
+import path from "path";
+import prisma from "@/lib/prisma";
+import { put } from "@vercel/blob";
+import { string } from "zod";
+import { redirect } from "next/navigation";
+
+export default async function createJobPosting(formData: FormData) {
+  const values = Object.fromEntries(formData.entries());
+
+  const {
+    title,
+    type,
+    companyName,
+    companyLogo,
+    locationType,
+    location,
+    applicationEmail,
+    applicationUrl,
+    description,
+    salary,
+  } = CreateJobSchema.parse(values);
+
+  const slug = `${toSlug(title)}-${nanoid(10)}}`;
+
+  let companyLogoUrl: string | undefined;
+
+  if (companyLogo) {
+    const blob = await put(
+      `company_logos/${slug}${path.extname(companyLogo.name)}`,
+      companyLogo,
+      {
+        access: "public",
+        addRandomSuffix: false,
+      }
+    );
+    companyLogoUrl = blob.url;
+  }
+
+  await prisma.job.create({
+    data: {
+      slug: slug,
+      title,
+      type,
+      companyName: companyName.trim(),
+      companyLogoUrl,
+      locationType,
+      location,
+      applicationEmail: applicationEmail?.trim(),
+      applicationUrl: applicationUrl?.trim(),
+      description: description?.trim(),
+      salary: parseInt(salary),
+    },
+  });
+
+  redirect("/job-submitted");
+}
